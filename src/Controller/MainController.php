09456ba7012941d456022b82bd6f8b60cdd259f7
XavierDepoortere <depoorx@gmail.com>
2023-08-17 00:25:56 +0200

    ajout code pour recu la liste de tt ls participant

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ParticipantRepository $repo): Response
     {
+
+        $liste = $repo->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_name' => 'MainController','liste_twig' => $liste,
         ]);
     }
 }
