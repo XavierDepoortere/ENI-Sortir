4f5e9e07c48620f9e52b5f10adc9619a17ac9c81
XavierDepoortere <depoorx@gmail.com>
2023-08-17 17:32:55 +0200

    ignore mot de passe edit profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,32 +4,53 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ParticipantType;
-use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ParticipantRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ParticipantController extends AbstractController
 {
     #[Route('/profil/{id}', name: 'app_profil')]
-    public function profil(  Participant $participant, Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Participant $participant, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
-       $form = $this->createForm(ParticipantType::class, $participant);
+        // Vérification de l'autorisation
+        if ($this->getUser() !== $participant) {
+            return $this->redirectToRoute('app_main');
+        }
 
-       $form->handleRequest($request);
+        $form = $this->createForm(ParticipantType::class, $participant);
 
-      $entityManager->persist($participant);
+        $originalPassword = $participant->getPassword();
 
-      $entityManager->flush();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer le champ de mot de passe depuis le formulaire
+            $motPasseField = $form->get('motPasse');
 
+            // Vérifier si le champ a été modifié et n'est pas vide
+            if ($motPasseField->isSubmitted() && !$motPasseField->isEmpty() && !$motPasseField->isDisabled()) {
+                $plainPassword = $motPasseField->getData();
+                $hashedPassword = $passwordHasher->hashPassword($participant, $plainPassword);
+                $participant->setPassword($hashedPassword);
+            } else {
+                // Si le champ n'a pas été modifié, restaurer le mot de passe original
+                $participant->setPassword($originalPassword);
+            }
+
+            // Enregistrer les modifications
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_main');
+        }
 
         return $this->render('participant/profil.html.twig', [
-            "participantForm"=> $form->createView(),
-
+            'participantForm' => $form->createView(),
         ]);
     }
 }
-
--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -4,39 +4,44 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use App\Entity\Participant;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class ParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('pseudo')
-            ->add('telephone')
-
-            ->add('campus', EntityType::class, [
-
-                'class' => Campus::class,
-
-                'choice_label' => 'nom'
-
-            ])
-
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Participant::class,
+        ->add('pseudo')
+        ->add('nom')
+        ->add('prenom')
+        ->add('telephone')
+        ->add('email')
+        ->add('campus', EntityType::class, [
+            'class' => Campus::class, 
+            'choice_label' => 'nom'
+        ])
+        ->add('motPasse', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'required' => false,
+            'mapped' => false,
+            
+            'options' => ['attr' => ['class' => 'password-field']],
+            'first_options'  => ['label' => 'Password'],
+            'second_options' => ['label' => 'Repeat Password'],
         ]);
-    }
+}
+
+public function configureOptions(OptionsResolver $resolver): void
+{
+    $resolver->setDefaults([
+        'data_class' => Participant::class,
+    ]);
+}
 }
