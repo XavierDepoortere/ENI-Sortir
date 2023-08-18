3da0ed2ff7bf896885b838aab55fe2ca95235f84
XavierDepoortere <depoorx@gmail.com>
2023-08-18 09:07:46 +0200

    ajout profil courant

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\Campus;
 use Doctrine\ORM\EntityManager;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ParticipantRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,15 +16,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(ParticipantRepository $repo,EntityManagerInterface $entityManager): Response
+    public function index(SortieRepository $repo,EntityManagerInterface $entityManager): Response
     {
         $infoList = $entityManager->getRepository(Campus::class)->findAll();
 
-        $liste = $repo->findAll();
+        $listeSorties = $repo->findAll();
 
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController','liste_twig' => $liste,'infoList' => $infoList,
+            'controller_name' => 'MainController','liste_sorties' => $listeSorties,'infoList' => $infoList,
         ]);
     }
+    
 
 }
