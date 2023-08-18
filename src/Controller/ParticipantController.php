aaf294da1a89e57920483c8fe8750ff15dc2b59e
Antoine MARTIN <antoine.martin2023@campus-eni.fr>
2023-08-18 14:02:32 +0200

    page_404_message erreur

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,23 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ParticipantType;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\ParticipantRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ParticipantController extends AbstractController
 {
     #[Route('/profil/{id}', name: 'app_profil')]
     public function edit(?Participant $participant, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
-    {   //TODO TEST SI PARTICIPANT = 0 redirect to login  + add flash message ( ce participant  )
-        //TODO CODE POUR FAIRE EN SORT QUE SI ON INDIQUE UN ID INCONNU DANS LA BDD ON ARRIVE SUR UNE 404
-        //TODO TEST POUR NUMERIQUE OBLIGATOIRE POUR L'ID DU PARTICIPANT
-        //TODO faire un deuxieme template pour affichage d'un et ou de monprofil
+    {   
+
+        // Vérifier si le participant est null, c'est-à-dire que l'ID est invalide
+        // if (!$participant) {
+        //     return $this->redirectToRoute('app_erreur');
+        // }
+        
         $form = $this->createForm(ParticipantType::class, $participant);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -39,5 +41,7 @@ class ParticipantController extends AbstractController
         return $this->render('participant/profil.html.twig', [
             'participantForm' => $form->createView(),'participant' => $participant,
         ]);
+    
     }
-}
+
+}   
