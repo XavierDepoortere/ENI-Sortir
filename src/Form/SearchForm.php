72c64b862262b81395b763c82b9c8b35ab56f318
Antoine MARTIN <antoine.martin2023@campus-eni.fr>
2023-11-02 10:36:25 +0100

    modified

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -20,7 +20,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class SearchForm extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options,)
+    public function buildForm(FormBuilderInterface $builder, array $options,): void
     {
         $builder
         ->add('q', TextType::class, [
